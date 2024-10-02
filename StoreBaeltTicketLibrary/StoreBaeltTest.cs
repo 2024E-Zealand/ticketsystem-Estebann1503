4b27bf8b48d525fa7b57736e0d48179a72eb5f1f
Estebann1503 <144995626+Estebann1503@users.noreply.github.com>
2024-10-02 11:44:39 +0200

    StoreBaeltTicketLibrary added using DLL

---
+++ StoreBaeltTicketLibrary/StoreBaeltTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TicketClassLibrary; // Namespace from the DLL
+
+namespace StoreBaeltTicketLibrary
+{
+    public class StoreBaeltTest
+    {
+        public void TestMethod()
+        {
+            Car car = new Car
+            {
+                Licenseplate = "SB12345",
+                Date = DateTime.Now,
+                BrobizzUsed = true
+            };
+
+            Console.WriteLine($"Vehicle Type: {car.VehicleType()}");
+            Console.WriteLine($"Price: {car.Price()}");
+        }
+    }
+}
